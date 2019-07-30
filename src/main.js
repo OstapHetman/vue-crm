3dc824387d095f9ae9893f3222bf688d909f680e
OstapHetman <prickly4528@gmail.com>
2019-07-30 11:12:52 +0300

    login with firebase

--- src/main.js
+++ src/main.js
@@ -1,17 +1,41 @@
 import Vue from 'vue'
+import Vuelidate from 'vuelidate'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 import dateFilter from '@/filters/date.filter'
+import messagePlugin from '@/utils/message.plugin'
 import './registerServiceWorker'
 import 'materialize-css/dist/js/materialize.min'
 
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/database'
+
 Vue.config.productionTip = false
 
+Vue.use(Vuelidate)
+Vue.use(messagePlugin)
 Vue.filter('date', dateFilter)
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+firebase.initializeApp({
+  apiKey: "AIzaSyBKCDWkvyF5P_xuTAVNLZ487osWkV-CDKQ",
+  authDomain: "crm-budget.firebaseapp.com",
+  databaseURL: "https://crm-budget.firebaseio.com",
+  projectId: "crm-budget",
+  storageBucket: "",
+  messagingSenderId: "927873310971",
+  appId: "1:927873310971:web:9ca39ea022f97aac"
+});
+
+let app;
+
+firebase.auth().onAuthStateChanged(() => {
+  if (!app) {
+    app = new Vue({
+      router,
+      store,
+      render: h => h(App)
+    }).$mount('#app')
+  }
+})
