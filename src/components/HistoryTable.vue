7a8811cd1bc1f4b4404e3f485e4c58ca06c616cd
OstapHetman <prickly4528@gmail.com>
2019-07-31 17:49:15 +0300

    show all records in table

---
+++ src/components/HistoryTable.vue
@@ -0,0 +1,47 @@
+<template>
+  <table>
+    <thead>
+      <tr>
+        <th>#</th>
+        <th>Сума</th>
+        <th>Дата</th>
+        <th>Категорія</th>
+        <th>Тип</th>
+        <th>Детальніше</th>
+      </tr>
+    </thead>
+
+    <tbody>
+      <tr v-for="(record, index) of records" :key="index">
+        <td>{{ index+1 }}</td>
+        <td>{{ record.amount | currency('UAH')}}</td>
+        <td>{{ record.date | date('datetime') }}</td>
+        <td>{{ record.CategoryName }}</td>
+        <td>
+          <span class="white-text badge" :class="[record.typeClass]">{{ record.typeText }}</span>
+        </td>
+        <td>
+          <button
+            v-tooltip="'Переглянути запис'"
+            class="btn-small btn"
+            @click="$router.push('/detail/' + record.id)"
+          >
+            <i class="material-icons">open_in_new</i>
+          </button>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+  props: {
+    records: {
+      required: true,
+      type: Array
+    }
+  }
+};
+</script>
+
